23787b07d54bfe3c43f2c66cee23db48c9521bdf
Brandon Rosero <yesidrosero2002@gmail.com>
2024-05-02 20:33:41 -0500

    primeros componentes

--- mercadoAgroecologicoFUP/src/app/app.module.ts
+++ mercadoAgroecologicoFUP/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/sesion/login/login.component';
+import { UserIndexComponent } from './components/user/user-index/user-index.component';
+import { ProductIndexComponent } from './components/product/product-index/product-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserIndexComponent,
+    ProductIndexComponent
   ],
   imports: [
     BrowserModule,
