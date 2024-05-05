d091e6be9f02289918c05d7e1c59f3045a887f14
Brandon Rosero <yesidrosero2002@gmail.com>
2024-05-05 16:03:10 -0500

    modificar login componente ts

--- mercadoAgroecologicoFUP/src/app/components/vistas/login/login.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/services/login/login.service';
 import { loginI } from 'src/app/models/login.interface';
 
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { responsiveI } from 'src/app/models/response.interface';
 
 @Component({
@@ -12,7 +12,6 @@ import { responsiveI } from 'src/app/models/response.interface';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
   loginForm = new FormGroup({
     use_cc: new FormControl('', Validators.required),
     use_password: new FormControl('', Validators.required),
@@ -21,19 +20,19 @@ export class LoginComponent {
   constructor(private loginS: LoginService, private router: Router) {}
 
   onLogin(form: loginI) {
-    this.loginS.loginCC(form).subscribe((data) => {
-      console.log(data)
-      let dataR: responsiveI = data;
-      console.log(dataR)
-  console.log(dataR.status);
-      if(!(data.status)){
-       // localStorage.setItem("token", dataR.token);
-       console.log(dataR)
-        this.router.navigate(['home'])
-      }else {
-        this.router.navigate(['category']);
-      }
+    this.loginS.loginCC(form).subscribe(
+      (data) => {
+        let dataR: responsiveI = data;
 
-    });
+        if (data.status) {
+          localStorage.setItem('token', dataR.token);
+          this.router.navigate(['home']);
+        }
+      },
+      (error) => {
+        // Aquí manejas el error alertas
+        alert('Error al intentar iniciar sesión:');
+      }
+    );
   }
 }
