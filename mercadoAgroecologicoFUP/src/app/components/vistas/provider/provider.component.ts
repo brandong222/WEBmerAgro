f332d3245256ade3b020881f8c81e6f2170a1e67
Ronald Rengifo <ronaldpptx@gmail.com>
2024-05-09 15:03:03 -0500

    ts de privider y json

--- mercadoAgroecologicoFUP/src/app/components/vistas/provider/provider.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/provider/provider.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProviderI } from 'src/app/models/provider.interface';
+import { ProviderService } from 'src/app/services/provider/provider.service';
+
 
 @Component({
   selector: 'app-provider',
   templateUrl: './provider.component.html',
   styleUrls: ['./provider.component.css']
 })
-export class ProviderComponent {
+export class ProviderComponent implements OnInit {
+
+  constructor(private providerS: ProviderService, private route: Router) { }
+
+  providerArray: ProviderI[] = [];
+
+  ngOnInit(): void {
+    this.providerS.getProvider().subscribe(data=>{
+      console.log(data);
+    })
+  }
 
 }
