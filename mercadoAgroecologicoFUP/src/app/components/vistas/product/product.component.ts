8efc7a9d063b342c549d8379b243027cac350665
Brandon Rosero <yesidrosero2002@gmail.com>
2024-05-08 19:01:23 -0500

    productos

--- mercadoAgroecologicoFUP/src/app/components/vistas/product/product.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/product/product.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ProductI } from './../../../models/product.interface';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  //primero se crea arreglo usando interfaz de product
+  products: ProductI[] = [];
+  //segundo se inyecta las dependencias de servicio y rutas
+  constructor(private productS: ProductService, private route: Router) {}
 
+  ngOnInit(): void {
+    this.productS.getproductos().subscribe(data=>{
+      console.log(data);
+    })
+  }
 }
--- mercadoAgroecologicoFUP/src/app/services/product/product.service.ts
+++ mercadoAgroecologicoFUP/src/app/services/product/product.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ProductI } from 'src/app/models/product.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private apiUrl: string = 'http://127.0.0.1:8000/api/product';
+
+  //para traer datos de la api
+  getproductos(): Observable<ProductI[]> {
+    const token = localStorage.getItem('token');
+    const headers = { Authorization: `Bearer ${token}` };
+    return this.http.get<ProductI[]>(this.apiUrl, { headers });
+  }
 }
