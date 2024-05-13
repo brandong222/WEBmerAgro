6381a11d752a548a7f1e499efb7ff90353aa1627
Ronald Rengifo <ronaldpptx@gmail.com>
2024-05-13 12:58:57 -0500

    html, css, ts provedores

--- mercadoAgroecologicoFUP/src/app/components/vistas/provider/provider.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/provider/provider.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProviderI } from 'src/app/models/provider.interface';
 import { ProviderService } from 'src/app/services/provider/provider.service';
+import { PeopleService } from 'src/app/services/people/people.service';
+import { By } from '@angular/platform-browser';
+import { PeopleI } from 'src/app/models/people.interface';
 
 
 @Component({
@@ -11,15 +14,24 @@ import { ProviderService } from 'src/app/services/provider/provider.service';
 })
 export class ProviderComponent implements OnInit {
 
-  constructor(private providerS: ProviderService, private route: Router) { }
+  constructor(private providerS: ProviderService, private route: Router, private peopleServide: PeopleService  ) { }
 
   providerArray: ProviderI[] = [];
+  peopleData: PeopleI[] = [];
 
   ngOnInit(): void {
-    this.providerS.getProvider().subscribe(data=>{
+    this.providerS.getProvider().subscribe(data => {
       console.log(data);
       this.providerArray = data;
-    })
+    });
+    this.peopleServide.getPeople().subscribe(peopleData => {
+      // Asumiendo que peopleData es un array de personas
+      this.peopleData = peopleData.map(persona => {
+        // Buscar el proveedor correspondiente por people_id
+        const provider = this.providerArray.find(p => p.people_peo_id === persona.id);
+        // Devolver un nuevo objeto que incluya la descripción del proveedor
+        return {...persona, prov_description: provider? provider.prov_description : 'Descripción no disponible'};
+      });
+    });
   }
-
 }
--- mercadoAgroecologicoFUP/src/app/models/provider.interface.ts
+++ mercadoAgroecologicoFUP/src/app/models/provider.interface.ts
@@ -1,6 +1,6 @@
 export interface ProviderI {
-  id?: number | null;
-  prov_ranking?: number | null;
+ id?: number | null;
+ prov_ranking?: number | null;
  prov_group: string | null;
  prov_description: string | null;
  prov_status: boolean | null;
