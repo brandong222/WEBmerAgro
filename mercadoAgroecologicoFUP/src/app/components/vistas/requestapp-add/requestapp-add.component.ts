17142e24f89161ba608a6535021c981f366f07d0
Ronald Rengifo <ronaldpptx@gmail.com>
2024-05-17 11:50:53 -0500

    html, css, ts requesr/add

--- mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-add/requestapp-add.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-add/requestapp-add.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-requestapp-add',
   templateUrl: './requestapp-add.component.html',
   styleUrls: ['./requestapp-add.component.css']
 })
-export class RequestappADDComponent {
+export class RequestappADDComponent  implements OnInit{
+  ngOnInit(): void {
+
+  }
+
+  tipoSolicitud: string = '';
+
+  onTipoSolicitudChange(): void {
+  }
 
 }
