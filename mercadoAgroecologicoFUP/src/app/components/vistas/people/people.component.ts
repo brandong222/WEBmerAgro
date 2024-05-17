e46e3599f7e78b7c4c1dc4901021c663a35123aa
Brandon Rosero <yesidrosero2002@gmail.com>
2024-05-17 11:49:54 -0500

    people funcional

--- mercadoAgroecologicoFUP/src/app/components/vistas/people/people.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/people/people.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PeopleI } from 'src/app/models/people.interface';
+import { UserI } from 'src/app/models/user.interface';
+import { JoinService } from 'src/app/services/join/join.service';
 import { PeopleService } from 'src/app/services/people/people.service';
 
 @Component({
@@ -8,18 +10,36 @@ import { PeopleService } from 'src/app/services/people/people.service';
   styleUrls: ['./people.component.css'],
 })
 export class PeopleComponent implements OnInit {
-  peopleArray: PeopleI[] = [];
+  peopleArray: any[] = [];
 
-  constructor(private peopleS: PeopleService) {}
+  constructor(
+    private peopleS: PeopleService,
+    private fkjoinS: JoinService
+  ) {}
 
   ngOnInit(): void {
+
     this.mostrarPeople();
   }
 
-  mostrarPeople() {
-    this.peopleS.getPeople().subscribe((data) => {
-     console.log(data);
-      this.peopleArray = data;
-    });
+
+  mostrarPeople(){
+    const user_id: UserI = this.traerDatosSesion();
+
+    this.fkjoinS.getjoinUserPeople(Number(user_id.id)).subscribe(data=>{
+      console.log(data.data)
+      this.peopleArray = data.data;
+    })
   }
+
+
+  traerDatosSesion(){
+    const usuarioData = sessionStorage.getItem('usuario_login');
+
+    if (usuarioData) {
+      return JSON.parse(usuarioData);
+    }
+  }
+
+
 }
