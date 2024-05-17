20320f3e3e798b5eb6e78904ce21b50cd098e3e5
Ronald Rengifo <ronaldpptx@gmail.com>
2024-05-17 17:12:50 -0500

    html, css, ts de request-add

--- mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-add/requestapp-add.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-add/requestapp-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { superInterfazI } from 'src/app/models/superInterfaz.interface';
+import { RequestappService } from 'src/app/services/requestapp/requestapp.service';
+import { RequestI } from 'src/app/models/request.interface';
+import { JoinService } from 'src/app/services/join/join.service';
+import { UserI } from 'src/app/models/user.interface';
 
 @Component({
   selector: 'app-requestapp-add',
@@ -6,8 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./requestapp-add.component.css']
 })
 export class RequestappADDComponent  implements OnInit{
-  ngOnInit(): void {
+  requestArray: superInterfazI[] = [];
 
+  constructor(
+    private requestS: RequestappService,
+    private fkjoinS: JoinService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+  this.mostrarRequest();
+  }
+
+  mostrarRequest(){
+    const request_id: RequestI = this.traerDatosSesion();
+    const user_id: UserI = this.traerDatosSesion();
+
+    this.fkjoinS.getjoinUserPeople(Number(user_id.id)).subscribe(data=>{
+      console.log(data.data)
+      this.requestArray = data.data;
+    })
   }
 
   tipoSolicitud: string = '';
@@ -15,4 +39,10 @@ export class RequestappADDComponent  implements OnInit{
   onTipoSolicitudChange(): void {
   }
 
+  traerDatosSesion(){
+    const requestData = sessionStorage.getItem('usuario_login')
+    if (requestData) {
+      return JSON.parse(requestData)
+    }
+  }
 }
