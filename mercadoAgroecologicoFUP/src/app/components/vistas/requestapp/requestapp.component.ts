26810d511abce9517d812ccfff21907f40081c64
Ronald Rengifo <ronaldpptx@gmail.com>
2024-05-22 12:06:46 -0500

    html, css, ts request

--- mercadoAgroecologicoFUP/src/app/components/vistas/requestapp/requestapp.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/requestapp/requestapp.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RequestI } from 'src/app/models/request.interface';
 import { RequestappService } from 'src/app/services/requestapp/requestapp.service';
+import { JoinService } from 'src/app/services/join/join.service';
 
 @Component({
   selector: 'app-requestapp',
@@ -9,14 +10,21 @@ import { RequestappService } from 'src/app/services/requestapp/requestapp.servic
   styleUrls: ['./requestapp.component.css'],
 })
 export class RequestappComponent implements OnInit {
-  constructor(private requestS: RequestappService, private route: Router) {}
+  constructor(private requestS: RequestappService,
+    private route: Router,
+    private FK_joinS: JoinService ) {}
 
-  requestArray: RequestI[] = [];
+  requestArray: any[] = [];
 
   ngOnInit(): void {
-    this.requestS.getRequestapp().subscribe((data) => {
-      console.log(data);
-      this.requestArray = data;
+       this.datosUnidos();
+  }
+
+  datosUnidos(){
+    this.FK_joinS.showReqPeoUsu().subscribe((info) => {
+      console.log(info.status);
+      console.log(info);
+      this.requestArray = info.data;
     });
   }
 }
