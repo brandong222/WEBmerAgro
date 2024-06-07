2a4455befea49cb22c8cb89b337eef168cf10602
Ronald Rengifo <ronaldpptx@gmail.com>
2024-06-07 10:24:35 -0500

    ts, html de request/add

--- mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-add/requestapp-add.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-add/requestapp-add.component.ts
@@ -13,7 +13,7 @@ import Swal from 'sweetalert2';
   templateUrl: './requestapp-add.component.html',
   styleUrls: ['./requestapp-add.component.css']
 })
-export class RequestappADDComponent  implements OnInit{
+export class RequestappADDComponent implements OnInit {
   requestArray: superInterfazI[] = [];
   requestForm: FormGroup;
 
@@ -27,36 +27,52 @@ export class RequestappADDComponent  implements OnInit{
       req_dateRequest: new FormControl('', Validators.required),
       req_type: new FormControl('', Validators.required),
       req_description: new FormControl('', Validators.required),
-      req_status: new FormControl(0 , Validators.required),
-      people_id: new FormControl(0 ,Validators.required),
+      req_status: new FormControl(0, Validators.required),
+      people_id: new FormControl(0, Validators.required),
     });
 
   }
 
   ngOnInit(): void {
-  this.mostrarRequest();
+    this.mostrarRequest();
   }
 
-  mostrarRequest(){
+  mostrarRequest() {
     const request_id: superInterfazI = this.traerDatosSesion();
     const user_id: UserI = this.traerDatosSesion();
 
-    this.fkjoinS.getjoinUserPeople(Number(user_id.id)).subscribe(data=>{
+    this.fkjoinS.getjoinUserPeople(Number(user_id.id)).subscribe(data => {
       console.log(data.data)
       this.requestArray = data.data;
       this.requestForm.get('people_id')?.setValue(request_id.people_id || null);
-      this.requestForm.get('req_status')?.setValue(request_id.use_status || null);
+      this.requestForm.get('req_status')?.setValue(1 || null);
 
-      if(request_id.use_rol === 'usuario'){
+      if (request_id.use_rol === 'usuario') {
         this.requestForm.get('req_type')?.setValue("Ser productor");
-      }else if (request_id.use_rol === 'productor'){
+      } else if (request_id.use_rol === 'productor') {
         this.requestForm.get('req_type')?.setValue("Certificar producto (s)");
       }
+      this.setTodayDate();
     })
   }
 
+  //Traer fecha
+  getTodayDate(): string {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // Los meses en JavaScript empiezan en 0!
+    const yyyy = today.getFullYear();
 
-  traerDatosSesion(){
+    return `${yyyy}-${mm}-${dd}`;
+  }
+
+  //Poner fecha
+  setTodayDate() {
+    this.requestForm.get('req_dateRequest')?.setValue(this.getTodayDate(), { emitEvent: false }); // Opcionalmente, puedes evitar emitir el evento si no es necesario
+  }
+
+
+  traerDatosSesion() {
     const requestData = sessionStorage.getItem('usuario_login')
     if (requestData) {
       return JSON.parse(requestData)
@@ -84,10 +100,12 @@ export class RequestappADDComponent  implements OnInit{
       },
       (error) => {
         // Aquí manejas el error alertas
-        alert('no se guardo');
+        Swal.fire(
+          'Ingrese descripción',
+        )
       }
     );
 
-}
+  }
 
 }
