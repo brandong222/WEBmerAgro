33c9f1bd8cc20749e83cad6f00811bcd18520558
Brandon Rosero <yesidrosero2002@gmail.com>
2024-06-07 09:53:12 -0500

    ver solicitudes modificaciones

--- mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-edit/requestapp-edit.component.ts
+++ mercadoAgroecologicoFUP/src/app/components/vistas/requestapp-edit/requestapp-edit.component.ts
@@ -9,20 +9,17 @@ import { ProductService } from 'src/app/services/product/product.service';
 import { ProviderService } from 'src/app/services/provider/provider.service';
 import { RequestappService } from 'src/app/services/requestapp/requestapp.service';
 
-
 @Component({
   selector: 'app-requestapp-edit',
   templateUrl: './requestapp-edit.component.html',
-  styleUrls: ['./requestapp-edit.component.css']
+  styleUrls: ['./requestapp-edit.component.css'],
 })
 export class RequestappEDITComponent {
-
   peopleArray: superInterfazI[] = [];
   peopleForm: FormGroup;
-  link_imagen_people:string = ""
+  link_imagen_people: string = '';
   id_productor_ruta: number = Number(this.routeA.snapshot.paramMap.get('id'));
   id_people_fk: number = 0;
-
 
   constructor(
     private route: Router,
@@ -31,35 +28,28 @@ export class RequestappEDITComponent {
     private fkjoinS: JoinService,
     private providerS: ProviderService,
     private productS: ProductService,
-    private requestS: RequestappService,
-
-  ){
+    private requestS: RequestappService
+  ) {
     this.peopleForm = new FormGroup({
       id: new FormControl('', Validators.required),
       peo_name: new FormControl('', Validators.required),
-      peo_lastName: new FormControl("", Validators.required),
+      peo_lastName: new FormControl('', Validators.required),
       peo_adress: new FormControl('', Validators.required),
       peo_dateBirth: new FormControl(null),
       peo_image: new FormControl('', Validators.required),
       peo_mail: new FormControl('', Validators.required),
       peo_phone: new FormControl(0, Validators.required),
     });
-
   }
 
   //obtener id de people
-  obtenerPeopleID(){
-    this.requestS.getRequestAppId(this.id_productor_ruta).subscribe(data=>{
-    //traer id de la persona
-      this.providerS.getProviderId(Number(data.data.people_id)).subscribe(datos=>{
-        console.log(datos.data.people_peo_id);
-      this.id_people_fk = datos.data.people_peo_id;
+  obtenerPeopleID() {
+    this.requestS.getRequestAppId(this.id_productor_ruta).subscribe((data) => {
+      this.id_people_fk = data.data.people_id;
       //metodo para buscar datos
       this.datosPeople();
-      })
     });
   }
-
 
   datosPeople() {
     //metodo para tomar los datos guardados y ponerlos en producto que se decea editar
@@ -68,16 +58,19 @@ export class RequestappEDITComponent {
       if (data) {
         const dataR: PeopleI = data.data;
 
-       this.peopleForm.get('id')?.setValue(Number(dataR.id));
+        this.peopleForm.get('id')?.setValue(Number(dataR.id));
         this.peopleForm.get('peo_name')?.setValue(dataR.peo_name || null);
-        this.peopleForm.get('peo_lastName')?.setValue(dataR.peo_lastname || null);
+        this.peopleForm
+          .get('peo_lastName')
+          ?.setValue(dataR.peo_lastname || null);
         this.peopleForm.get('peo_adress')?.setValue(dataR.peo_adress || null);
-        this.peopleForm.get('peo_dateBirth')?.setValue(dataR.peo_dateBirth || null);
+        this.peopleForm
+          .get('peo_dateBirth')
+          ?.setValue(dataR.peo_dateBirth || null);
         this.peopleForm.get('peo_image')?.setValue(dataR.peo_image || null);
         this.link_imagen_people = String(dataR.peo_image); //usado para poner la imagen en un img
         this.peopleForm.get('peo_mail')?.setValue(dataR.peo_mail || null);
         this.peopleForm.get('peo_phone')?.setValue(dataR.peo_phone || null);
-
       } else {
         console.log('No hay datos guardados del productor.');
       }
@@ -85,12 +78,11 @@ export class RequestappEDITComponent {
   }
 
   ngOnInit(): void {
-  this.obtenerPeopleID();
+    this.obtenerPeopleID();
   }
 
   //NAvegacion
-  nvRegresar(){
+  nvRegresar() {
     this.route.navigate(['/request']);
   }
-
 }
